930b3ae4cb8f6b0fb13bf26b34bc1957658e7fb0
Evan Miller <22ermiller@gmail.com>
2026-07-02 10:45:58 -0600

    data file updates

--- data_cleaning/cpi.R
+++ data_cleaning/cpi.R
@@ -1,12 +1,16 @@
 library(tidyverse)
 library(janitor)
+library(readxl)
 
 # Employment Cost Index --------------------------------------------------
 
-# link to data: https://fred.stlouisfed.org/series/ECIWAG
+# link to data: https://www.bls.gov/eci/tables.htm
 
 # read in  data
-eci_raw <- read_csv("raw_data/eci-continuous-dataset.csv") |>
+eci_raw <- read_xlsx(
+  "raw_data/eci-continuous-dataset.xlsx",
+  sheet = "Continuous dataset"
+) |>
   clean_names()
 
 
@@ -18,7 +22,9 @@ eci_final <- eci_raw |>
       estimate_type == "Total compensation" &
       ownership == "Civilian workers" # all workers
   ) |>
-  mutate(date = as.Date(paste(year, period, "1", sep = "-"), format = "%Y-%B-%d")) |>
+  mutate(
+    date = as.Date(paste(year, period, "1", sep = "-"), format = "%Y-%B-%d")
+  ) |>
   select(date, eci = estimate)
 
 write_csv(eci_final, "data/eci.csv")
@@ -26,13 +32,18 @@ write_csv(eci_final, "data/eci.csv")
 
 # Medical Inflation ------------------------------------------------------
 
-# link to data: https://fred.stlouisfed.org/series/CPIMEDSL
+# link to data: https://data.bls.gov/timeseries/CUUR0000SAM?output_view=data
+# for correctly formatted select earliest date range (1935), click "More Formatting Options", and then click "Column Format" then "Retreive Data"
 
-med_inflation_raw <- read_csv("raw_data/medical_inflation_raw.csv") |> 
+med_inflation_raw <- read_csv("raw_data/medical_inflation_raw.csv") |>
   clean_names()
 
-med_inflation_final <- med_inflation_raw |> 
-  mutate(date = lubridate::ym(label)) |> 
+med_inflation_final <- med_inflation_raw |>
+  filter(str_starts(period, "M")) %>% # keep monthly rows only
+  mutate(
+    month = as.integer(str_remove(period, "M")),
+    date = make_date(year, month, 1) # first of the month
+  ) |>
   select(date, med_inflation = value)
 
 write_csv(med_inflation_final, "data/med_inflation.csv")
@@ -42,9 +53,9 @@ write_csv(med_inflation_final, "data/med_inflation.csv")
 
 # link to data: https://fred.stlouisfed.org/series/CUSR0000SA0L5
 
-cpi_raw <- read_csv("raw_data/cpi_less_medical_raw.csv") |> 
+cpi_raw <- read_csv("raw_data/cpi_less_medical_raw.csv") |>
   clean_names()
 
-cpi_final <- cpi_raw |> rename(cpi = cusr0000sa0l5)
+cpi_final <- cpi_raw |> rename(date = observation_date, cpi = cusr0000sa0l5)
 
 write_csv(cpi_final, "data/cpi.csv")
--- data_cleaning/interest_rates.R
+++ data_cleaning/interest_rates.R
@@ -3,20 +3,20 @@ library(tidyverse)
 
 # Format CPI so we know what dates to filter interest rates to ------------
 
-cpi_df_raw <- read_csv("data/cpi.csv") 
+cpi_df_raw <- read_csv("data/cpi.csv")
 
-cpi_df <- cpi_df_raw |> 
-  filter(date >= "1990-01-01") |> # only dates from 1990 onwards
-  mutate(lagged_cpi = lag(cpi, n = 1)) |> 
-  mutate(log_dif_cpi = log(cpi) - log(lagged_cpi)) |> 
-  filter(!is.na(log_dif_cpi))
+cpi_df <- cpi_df_raw |>
+       filter(date >= "1990-01-01") |> # only dates from 1990 onwards
+       mutate(lagged_cpi = lag(cpi, n = 1)) |>
+       mutate(log_dif_cpi = log(cpi) - log(lagged_cpi)) |>
+       filter(!is.na(log_dif_cpi))
 
 
 # Read in Interest Rates --------------------------------------------------
 
 # link to data: https://fred.stlouisfed.org/series/DGS3MO
 
-ir3mo_raw <- read_csv("raw_data/DGS3MO (1).csv")
+ir3mo_raw <- read_csv("raw_data/DGS3MO.csv")
 
 # link to data: https://fred.stlouisfed.org/series/DGS10
 
@@ -30,7 +30,7 @@ ir20yr_raw <- read_csv("raw_data/DGS20.csv")
 
 ir1yr_raw <- read_csv("raw_data/DGS1.csv")
 
-ir2yr_raw <- read_csv("raw_data/DGS2(1).csv")
+ir2yr_raw <- read_csv("raw_data/DGS2.csv")
 
 ir3yr_raw <- read_csv("raw_data/DGS3.csv")
 
@@ -40,152 +40,142 @@ ir7yr_raw <- read_csv("raw_data/DGS7.csv")
 
 # Format Data -------------------------------------------------------------
 
-ir3mo <- ir3mo_raw |> 
-  rename(date = observation_date,
-         rate = DGS3MO) |> 
-  filter(!is.na(rate)) |> 
-  mutate(year = year(date),
-         month = month(date),
-         day = day(date)) |> 
-  group_by(year, month) |> 
-  arrange(day) |> 
-  slice_head(n = 1) |> 
-  ungroup() |> 
-  mutate(date = floor_date(date, unit = "month")) |>
-  filter(date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)) |> 
-  dplyr::select(date, rate)
+ir3mo <- ir3mo_raw |>
+       rename(date = observation_date, rate = DGS3MO) |>
+       filter(!is.na(rate)) |>
+       mutate(year = year(date), month = month(date), day = day(date)) |>
+       group_by(year, month) |>
+       arrange(day) |>
+       slice_head(n = 1) |>
+       ungroup() |>
+       mutate(date = floor_date(date, unit = "month")) |>
+       filter(
+              date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)
+       ) |>
+       dplyr::select(date, rate)
 
-ir10yr <- ir10yr_raw |> 
-  rename(date = observation_date,
-         rate = DGS10) |> 
-  filter(!is.na(rate)) |> 
-  mutate(year = year(date),
-         month = month(date),
-         day = day(date)) |> 
-  group_by(year, month) |> 
-  arrange(day) |> 
-  slice_head(n = 1) |> 
-  ungroup() |> 
-  mutate(date = floor_date(date, unit = "month")) |>
-  filter(date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)) |> 
-  dplyr::select(date, rate)
+ir10yr <- ir10yr_raw |>
+       rename(date = observation_date, rate = DGS10) |>
+       filter(!is.na(rate)) |>
+       mutate(year = year(date), month = month(date), day = day(date)) |>
+       group_by(year, month) |>
+       arrange(day) |>
+       slice_head(n = 1) |>
+       ungroup() |>
+       mutate(date = floor_date(date, unit = "month")) |>
+       filter(
+              date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)
+       ) |>
+       dplyr::select(date, rate)
 
-ir30yr <- ir30yr_raw |> 
-  rename(date = observation_date,
-         rate = DGS30) |> 
-  filter(!is.na(rate)) |> 
-  mutate(year = year(date),
-         month = month(date),
-         day = day(date)) |> 
-  group_by(year, month) |> 
-  arrange(day) |> 
-  slice_head(n = 1) |> 
-  ungroup() |> 
-  mutate(date = floor_date(date, unit = "month")) |>
-  filter(date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)) |> 
-  dplyr::select(date, rate)
+ir30yr <- ir30yr_raw |>
+       rename(date = observation_date, rate = DGS30) |>
+       filter(!is.na(rate)) |>
+       mutate(year = year(date), month = month(date), day = day(date)) |>
+       group_by(year, month) |>
+       arrange(day) |>
+       slice_head(n = 1) |>
+       ungroup() |>
+       mutate(date = floor_date(date, unit = "month")) |>
+       filter(
+              date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)
+       ) |>
+       dplyr::select(date, rate)
 
-ir1yr <- ir1yr_raw |> 
-  rename(date = observation_date,
-         rate = DGS1) |> 
-  filter(!is.na(rate)) |> 
-  mutate(year = year(date),
-         month = month(date),
-         day = day(date)) |> 
-  group_by(year, month) |> 
-  arrange(day) |> 
-  slice_head(n = 1) |> 
-  ungroup() |> 
-  mutate(date = floor_date(date, unit = "month")) |>
-  filter(date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)) |> 
-  dplyr::select(date, rate)
+ir1yr <- ir1yr_raw |>
+       rename(date = observation_date, rate = DGS1) |>
+       filter(!is.na(rate)) |>
+       mutate(year = year(date), month = month(date), day = day(date)) |>
+       group_by(year, month) |>
+       arrange(day) |>
+       slice_head(n = 1) |>
+       ungroup() |>
+       mutate(date = floor_date(date, unit = "month")) |>
+       filter(
+              date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)
+       ) |>
+       dplyr::select(date, rate)
 
 ir2yr <- ir2yr_raw |>
-  rename(date = observation_date,
-         rate = DGS2) |> 
-  filter(!is.na(rate)) |> 
-  mutate(year = year(date),
-         month = month(date),
-         day = day(date)) |> 
-  group_by(year, month) |> 
-  arrange(day) |> 
-  slice_head(n = 1) |> 
-  ungroup() |> 
-  mutate(date = floor_date(date, unit = "month")) |>
-  filter(date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)) |> 
-  dplyr::select(date, rate)
+       rename(date = observation_date, rate = DGS2) |>
+       filter(!is.na(rate)) |>
+       mutate(year = year(date), month = month(date), day = day(date)) |>
+       group_by(year, month) |>
+       arrange(day) |>
+       slice_head(n = 1) |>
+       ungroup() |>
+       mutate(date = floor_date(date, unit = "month")) |>
+       filter(
+              date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)
+       ) |>
+       dplyr::select(date, rate)
 
 ir3yr <- ir3yr_raw |>
-  rename(date = observation_date,
-         rate = DGS3) |> 
-  filter(!is.na(rate)) |> 
-  mutate(year = year(date),
-         month = month(date),
-         day = day(date)) |> 
-  group_by(year, month) |> 
-  arrange(day) |> 
-  slice_head(n = 1) |> 
-  ungroup() |> 
-  mutate(date = floor_date(date, unit = "month")) |>
-  filter(date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)) |> 
-  dplyr::select(date, rate)
+       rename(date = observation_date, rate = DGS3) |>
+       filter(!is.na(rate)) |>
+       mutate(year = year(date), month = month(date), day = day(date)) |>
+       group_by(year, month) |>
+       arrange(day) |>
+       slice_head(n = 1) |>
+       ungroup() |>
+       mutate(date = floor_date(date, unit = "month")) |>
+       filter(
+              date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)
+       ) |>
+       dplyr::select(date, rate)
 
 ir5yr <- ir5yr_raw |>
-  rename(date = observation_date,
-         rate = DGS5) |> 
-  filter(!is.na(rate)) |> 
-  mutate(year = year(date),
-         month = month(date),
-         day = day(date)) |> 
-  group_by(year, month) |> 
-  arrange(day) |> 
-  slice_head(n = 1) |> 
-  ungroup() |> 
-  mutate(date = floor_date(date, unit = "month")) |>
-  filter(date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)) |> 
-  dplyr::select(date, rate)
+       rename(date = observation_date, rate = DGS5) |>
+       filter(!is.na(rate)) |>
+       mutate(year = year(date), month = month(date), day = day(date)) |>
+       group_by(year, month) |>
+       arrange(day) |>
+       slice_head(n = 1) |>
+       ungroup() |>
+       mutate(date = floor_date(date, unit = "month")) |>
+       filter(
+              date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)
+       ) |>
+       dplyr::select(date, rate)
 
 ir7yr <- ir7yr_raw |>
-  rename(date = observation_date,
-         rate = DGS7) |> 
-  filter(!is.na(rate)) |> 
-  mutate(year = year(date),
-         month = month(date),
-         day = day(date)) |> 
-  group_by(year, month) |> 
-  arrange(day) |> 
-  slice_head(n = 1) |> 
-  ungroup() |> 
-  mutate(date = floor_date(date, unit = "month")) |>
-  filter(date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)) |> 
-  dplyr::select(date, rate)
+       rename(date = observation_date, rate = DGS7) |>
+       filter(!is.na(rate)) |>
+       mutate(year = year(date), month = month(date), day = day(date)) |>
+       group_by(year, month) |>
+       arrange(day) |>
+       slice_head(n = 1) |>
+       ungroup() |>
+       mutate(date = floor_date(date, unit = "month")) |>
+       filter(
+              date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)
+       ) |>
+       dplyr::select(date, rate)
 
 ir20yr <- ir20yr_raw |>
-  rename(date = observation_date,
-         rate = DGS20) |> 
-  filter(!is.na(rate)) |> 
-  mutate(year = year(date),
-         month = month(date),
-         day = day(date)) |> 
-  group_by(year, month) |> 
-  arrange(day) |> 
-  slice_head(n = 1) |> 
-  ungroup() |> 
-  mutate(date = floor_date(date, unit = "month")) |>
-  filter(date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)) |> 
-  dplyr::select(date, rate)
+       rename(date = observation_date, rate = DGS20) |>
+       filter(!is.na(rate)) |>
+       mutate(year = year(date), month = month(date), day = day(date)) |>
+       group_by(year, month) |>
+       arrange(day) |>
+       slice_head(n = 1) |>
+       ungroup() |>
+       mutate(date = floor_date(date, unit = "month")) |>
+       filter(
+              date >= min(cpi_df$date) - months(1) & date <= max(cpi_df$date)
+       ) |>
+       dplyr::select(date, rate)
 
 full_ir <- ir3mo |>
-  rename(three_month = rate) |> 
-  left_join(ir1yr |> rename(one_year = rate), by = "date") |>
-  left_join(ir2yr |> rename(two_year = rate), by = "date") |>
-  left_join(ir3yr |> rename(three_year = rate), by = "date") |>
-  left_join(ir5yr |> rename(five_year = rate), by = "date") |>
-  left_join(ir7yr |> rename(seven_year = rate), by = "date") |>
-  left_join(ir10yr |> rename(ten_year = rate), by = "date") |> 
-  left_join(ir20yr |> rename(twenty_year = rate), by = "date") |>
-  left_join(ir30yr |> rename(thirty_year = rate), by = "date")
-
+       rename(three_month = rate) |>
+       left_join(ir1yr |> rename(one_year = rate), by = "date") |>
+       left_join(ir2yr |> rename(two_year = rate), by = "date") |>
+       left_join(ir3yr |> rename(three_year = rate), by = "date") |>
+       left_join(ir5yr |> rename(five_year = rate), by = "date") |>
+       left_join(ir7yr |> rename(seven_year = rate), by = "date") |>
+       left_join(ir10yr |> rename(ten_year = rate), by = "date") |>
+       left_join(ir20yr |> rename(twenty_year = rate), by = "date") |>
+       left_join(ir30yr |> rename(thirty_year = rate), by = "date")
 
 
 # Save Data ---------------------------------------------------------------
@@ -200,4 +190,3 @@ write_csv(ir5yr, "data/ir5yr.csv")
 write_csv(ir7yr, "data/ir7yr.csv")
 write_csv(ir20yr, "data/ir20yr.csv")
 write_csv(full_ir, "data/full_ir.csv")
-
